229242fc1e9d9262374893a02920b5edb4148f5a
Abubakar <mianabubakar131998@gmail.com>
2019-09-21 19:53:40 +0500

    validations

--- Ajex(Crud Operations)/Controllers/AirlineController.cs
+++ Ajex(Crud Operations)/Controllers/AirlineController.cs
@@ -28,7 +28,7 @@ namespace Ajex_Crud_Operations_.Controllers
                 orm.SaveChanges();
                 ViewBag.hint = "Success";
                 ViewBag.message = airline.Name + " Data Saved Sucessfully";
-                return RedirectToAction("Create");
+                return View();
             }
             catch (Exception)
             {
@@ -37,13 +37,47 @@ namespace Ajex_Crud_Operations_.Controllers
                 return View();
             }
 
-        } 
-
+        }
+        [HttpGet]
         public IActionResult Detail()
         {
             return View(orm.Airline1.ToList());
         }
+        public string Delete(int id)
+        {
 
+            orm.Airline1.Remove(orm.Airline1.Find(id));
+            orm.SaveChanges();
+            return "1";
+        }
+        public IActionResult Details(int id)
+        {
+            return View(orm.Airline1.Find(id));
+        }
 
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var E_id = orm.Airline1.Find(id);
+            return View(E_id);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Airline1 airline)
+        {
+            orm.Airline1.Update(airline);
+            orm.SaveChanges();
+            return RedirectToAction("Detail");
+        }
+
+        public string uploadImage()
+        {
+            return "<img src='http://lorempixel.com/300/300/sports' />";
+        }
+        public string uploadVideo()
+        {
+            return "<iframe width='500' height='315' src='https://www.youtube.com/embed/yAIALB5VT2c' frameborder='0'" +
+                " allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+        }
     }
 }
--- Ajex(Crud Operations)/Models/Airline1.cs
+++ Ajex(Crud Operations)/Models/Airline1.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Ajex_Crud_Operations_.Models
 {
     public partial class Airline1
     {
+        [Required]
         public int Id { get; set; }
+        [MaxLength(5)]
         public string Name { get; set; }
         public string Country { get; set; }
         public string Status { get; set; }
